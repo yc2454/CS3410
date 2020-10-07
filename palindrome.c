38d485fca55dbfb58a129b8b29ee8033f3d76123
Yalu Cai <yc2454@cornell.edu>
2020-10-07 16:13:46 -0400

    first commit

--- palindrome.c
+++ palindrome.c
@@ -12,8 +12,31 @@ int main()
 
   // Find the length of the string.
   // Hint: How do you know when a string ends in C?
+  i = 0;
+  while (str[i] != '\0')
+  {
+    i++;
+  }
+  length = i;
 
   // Check if str is a palindrome.
+  start = 0;
+  end = length - 1;
+
+  while (start <= end)
+  {
+    if (str[start] != str[end])
+    {
+      flag = 1;
+      break;
+    }
+    else
+    {
+      flag = 0;
+      start++;
+      end--;
+    }
+  }
 
   if (flag == 1)
     printf("%s is not a palindrome.\n", str);
